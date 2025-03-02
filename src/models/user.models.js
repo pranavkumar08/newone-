83b3ab16c969f624568f9a518e926843c30ffefd
Pranav Kumar <pranavkrbihat22@gmail.com>
2025-03-02 13:34:03 +0530

    user schema created

---
+++ src/models/user.models.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+const userchema=new mongoose.Schema({
+name:{
+    type:String,
+    required:true
+},
+email:{
+    type:String,
+    required:true
+},
+password:{
+    type:String,
+    required:true
+}
+
+
+},{timestamps:true})
+
+export const User = mongoose.model("User",userchema)
